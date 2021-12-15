278a60790ab21a4d067e3e63c3be28fdbf31a0fe
sergio14camino <sergio14camino@gmail.com>
2021-12-14 23:09:50 -0500

    Añadiendo avances

--- sistem/siraq_prints.sql
+++ sistem/siraq_prints.sql
@@ -4,8 +4,10 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 CREATE TABLE `user` (
-  `id_user` int NOT NULL,
+  `id_user` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `nombre` varchar(300) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(300) NOT NULL,
   `descripción` text NOT NULL,
   `telefono` varchar(300) NOT NULL,
   `dirección` text NOT NULL,
@@ -15,7 +17,7 @@ CREATE TABLE `user` (
 
 
 CREATE TABLE `tags` (
-  `id_tag` int NOT NULL,
+  `id_tag` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name_tag` varchar(300) NOT NULL,
   `description_tag` text NOT NULL,
   `cover_tag` text NOT NULL
@@ -23,7 +25,7 @@ CREATE TABLE `tags` (
 
 
 CREATE TABLE `sheets` (
-  `id_sheets` int NOT NULL,
+  `id_sheets` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name_sheets` varchar(300) NOT NULL,
   `description_sheets` text NOT NULL,
   `price` int NOT NULL,
@@ -33,26 +35,19 @@ CREATE TABLE `sheets` (
 
 
 CREATE TABLE `relations` (
-  `id_relations` int NOT NULL,
+  `id_relations` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `id_tag` int NOT NULL,
   `id_sheets` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+INSERT INTO `user` 
+  (`id_user`, `nombre`, `email`, `password`, `descripción`, `telefono`, `dirección`, `imagen`) 
+VALUES
+  (1, 'andres', 'andres@gmail.com', 'andresc', 'Entusiasta', '978654321', 'La Molina', 'img'),
+  (2, 'julio', 'julio@gmail.com', 'julioc', 'Entusiasta', '978654321', 'Barranco', 'img');
 
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
 
-ALTER TABLE `tags`
-  ADD PRIMARY KEY (`id_tag`);
 
---
--- Indices de la tabla
---
-ALTER TABLE `sheets`
-  ADD PRIMARY KEY (`id_sheets`);
 
---
--- Indices de la tabla
---
-ALTER TABLE `relations`
-  ADD PRIMARY KEY (`id_relations`);
+
+  
