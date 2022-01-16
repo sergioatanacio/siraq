ba090cadb0533ddc7ae09e5c00a7e5b05efd7682
sergioAtanacio <sergio14camino@gmail.com>
2022-01-15 19:14:25 -0500

    Añadiendo el 404

--- app/routes.php
+++ app/routes.php
@@ -1,55 +1,27 @@
   
 
 <?php
-
+/**
+ * Es el archivo raíz.
+ */
 $routePrint('', 'generalController@temporary');
-$routePrint('apps', 'generalController@apps');
+
+/**
+ * Es la vista para iniciar sesión.
+ */
 $routePrint('user', 'generalController@user');
-$routePrint('temporaryAdmin', 'generalController@temporaryAdmin');
+
+/**
+ * Es la página donde se procesa el inicio de sessión.
+ */
 $routePrint('login_controller', 'generalController@login_controller');
-$routePrint('temporary_administrative_panel', 'generalController@temporary_administrative_panel');
 
+/**
+ * Es la ruta del panel administrativo.
+ */
+$routePrint('temporary_administrative_panel', 
+    'generalController@temporary_administrative_panel');
 
-
-
-
-
-
-
-
-$routePrint('administrative_panel', 'generalController@administrative_panel');
-
-
-
-
-
-$routePrint('table', 'generalController@start');
-$routePrint('script', 'generalController@script_start');
-$routePrint('students', 'generalController@students');
-$routePrint('courses', 'generalController@courses');
-$routePrint('json_course', 'generalController@json_course');
-$routePrint('json_students', 'generalController@json_students');
-$routePrint('json_inscriptions', 'generalController@json_inscriptions');
-$routePrint('inscriptions_insert', 'generalController@inscriptions_insert');
-$routePrint('table_simple', 'generalController@table_simple');
-$routePrint('style', 'stylesMis@stylePhp');
-
-
-$routePrint('listick', 'listick@beginning');
-$routePrint('login', 'listick@login');
-$routePrint('create_account', 'listick@create_account');
-
-
-
-$routePrint('siraq', 'siraq@beginning');
-$routePrint('load_products', 'siraq@load_products');
-$routePrint('load_products_push', 'siraq@load_products_push');
-
-
-$routePrint('younotes', 'younotes@beginning');
-$routePrint('younotes_script', 'younotes@younotes_script');
-$routePrint('younotes_login', 'younotes@younotes_login');
-$routePrint('younotes_create_account', 'younotes@younotes_create_account');
 
 
 
