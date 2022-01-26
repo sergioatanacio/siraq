6a9cf4eb955793a0df4893ade2cf76597fb9be5f
Your Name <you@example.com>
2022-01-25 22:06:54 -0500

    Antes de hacer todas las plantillas denuevo

--- public/scripts/temporaryAdmin.js
+++ public/scripts/temporaryAdmin.js
@@ -1,2 +1,16 @@
 //alert('Hola mundo');
-document.querySelector('#');
+/* fetch("/resourses/templates/templatesAdmin/sidebar.html")
+.then(response=>response.text())
+.then(data=>{
+    document.querySelector('#sidebar_id').innerHTML = data;
+    document.querySelector('#sidebar_id').appendChild = data;
+
+    fetch("/resourses/templates/template_body_login.html")
+    .then(response=>response.text())
+    .then(data=>{
+        document.querySelector('#bodyId').innerHTML = data;
+    })
+    .catch(err=>console.log(err))
+    
+})
+.catch(err=>console.log(err)) */
