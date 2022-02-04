30a2cbfc8d3eb5344db3af84fc86f9059f25379d
sergio14camino <sergio14camino@gmail.com>
2022-02-04 10:14:05 -0500

    Creando interfaz para api

--- public/test_api_for_fetch/query_first.js
+++ public/test_api_for_fetch/query_first.js
@@ -1,13 +1,68 @@
-//document.querySelector('#inicio').innerHTML = 'Hola mundo';
+//http://localhost:8000/test_api_for_fetch/test_api_for_fetch.html
 
-
+/* 
 fetch('http://localhost:8000/index.html')
-.then(uno => uno.text)
-.then(dos =>
+.then(response=> response.text())
+.then(text =>
     {
-        document.querySelector('#inicio').innerHTML = 'Hola mundo';
+        document.querySelector('#inicio').append(document.createTextNode(text));
     })
-.catch(tres=>
-        console.log(tres)
-    );
+.catch(tres=> console.log(tres) );
+ */
+
+
+
+const data = new FormData();
+data.append('email', 'andres@gmail.com');
+data.append('password', 'andresc');
+data.append('login_type', 'log_in');
+
+fetch('http://localhost:8000/login_controller', 
+{
+    method: 'POST',
+    body:   data,
+})
+.then(response=> response.text())
+.then(text =>
+{
+    document.querySelector('#inicio').append(document.createTextNode(text));
+})
+.catch(error => console.log(error));
+
+
+
+
+const session_existsData = new FormData();
+session_existsData.append('login_type', 'session_exists');
+fetch('http://localhost:8000/login_controller', 
+{
+    method: 'POST',
+    body:   session_existsData,
+})
+.then(response=> response.text())
+.then(text =>
+    {
+        document.querySelector('#inicio').append(document.createTextNode(text));
+    })
+.catch(tres=> console.log(tres) );
+
+
+
+/* 
+const close_sessionData = new FormData();
+close_sessionData.append('login_type', 'close_session');
+fetch('http://localhost:8000/login_controller', 
+{
+    method: 'POST',
+    body:   close_sessionData,
+})
+.then(response=> response.text())
+.then(text =>
+    {
+        document.querySelector('#inicio').append(document.createTextNode(text));
+    })
+.catch(tres=> console.log(tres) );
+
+ */
+
 
