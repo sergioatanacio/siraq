89f76caa830f7e9d14e2273ba05845b45d23ba0b
sergio14camino <sergio14camino@gmail.com>
2022-02-10 08:21:50 -0500

    Subiendo

--- public/test_api_for_fetch/test_api_page_start.js
+++ public/test_api_for_fetch/test_api_page_start.js
@@ -1,5 +1,11 @@
 
-let elemento = document.createElement('h1').append(document.createTextNode('Hola mundo'));
+let elemento = document.createElement('h1');
+elemento.append(document.createTextNode('Hola mundo'));
 //elemento.append();
 
 document.querySelector('#stamping_materials').append(elemento);
+
+let card_stamping_materials = document.querySelector('#template_stamping_materials').content;
+
+let img_stamping_materials = card_stamping_materials.querySelector('div img');
+img_stamping_materials.setAttribute('src', `/file_store/img_products${linck_image}`);
