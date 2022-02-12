48c40a99e992f3876695260685b9e49d5d9d9d2a
Your Name <you@example.com>
2022-02-12 14:10:48 -0500

    Enviando los datos y receteando el formulario.

---
+++ public/scripts/firs_index.js
@@ -0,0 +1,41 @@
+
+//let elemento = document.createElement('h1');
+//elemento.append(document.createTextNode('Hola mundo'));
+//elemento.append();
+//
+//document.querySelector('#stamping_materials').append(elemento);
+
+let data = new FormData();
+data.append('administrative_panel_type', 'get_stamping_materials');
+
+fetch('../administrative_panel', 
+{
+    method: 'POST',
+    body:   data,
+})
+.then(response=> response.json() )
+.then(text =>
+{
+    console.log(text);
+    let stamping_materials = document.querySelector('#stamping_materials');
+    let template_stamping_materials = document.querySelector('#template_stamping_materials').content;
+    let fragmet = document.createDocumentFragment();
+
+    text.forEach(element => {
+        console.log(element);
+        template_stamping_materials.querySelector('div div img').setAttribute('src', `/file_store/img_products/${element.material_images[0].linck_image}`);
+        template_stamping_materials.querySelector('div div img').setAttribute('alt', `hola mundo`);
+        template_stamping_materials.querySelector('div p').textContent = element.name_of_material;
+        /* True copia toda la estructura interna, si se le pasa un false, solo copia 
+        la etiqueta elegida (apertura y cierre)*/
+        let clone = document.importNode(template_stamping_materials, true)
+        fragmet.append(clone);
+    });
+    console.log(fragmet);
+    stamping_materials.append(fragmet);
+    
+})
+.catch(error => console.log(error));
+
+
+
