4cd0eba8502a18e06b483c9522fe7dd0fd3a8e0a
sergio14camino <sergio14camino@gmail.com>
2022-02-18 08:17:31 -0500

    Creando leer get en un página

--- public/scripts/firs_index.js
+++ public/scripts/firs_index.js
@@ -18,14 +18,15 @@ fetch('administrative_panel',
 {
     let template_stamping_materials = document.querySelector('#template_stamping_materials').content;
     let fragmet = document.createDocumentFragment();
-
+    
     text.forEach(element => {
         let linck_image = (element.material_images[0].linck_image) 
-            ? '/file_store/img_products/' + element.material_images[0].linck_image 
-            : '';
-        template_stamping_materials.querySelector('img').setAttribute('src', `${linck_image}`);
-        template_stamping_materials.querySelector('img').setAttribute('alt', `hola mundo`);
-        template_stamping_materials.querySelector('p').textContent = element.name_of_material;
+        ? '/file_store/img_products/' + element.material_images[0].linck_image 
+        : '';
+        template_stamping_materials.querySelector('.card_data_link').setAttribute('href', `single_stamping_material.html?id_stamping_size=${element.id_stamping_materials}`);
+        template_stamping_materials.querySelector('.card_image').setAttribute('src', `${linck_image}`);
+        template_stamping_materials.querySelector('.card_image').setAttribute('alt', `hola mundo`);
+        template_stamping_materials.querySelector('.name_card').textContent = element.name_of_material;
         /* True copia toda la estructura interna, si se le pasa un false, solo copia 
         la etiqueta elegida (apertura y cierre)*/
         let clone = document.importNode(template_stamping_materials, true)
@@ -57,9 +58,9 @@ fetch('administrative_panel',
         let linck_image = (element.stamping_size_image[0].linck_image) 
             ? '/file_store/img_products/' + element.stamping_size_image[0].linck_image 
             : '';
-        template_stamping_materials.querySelector('div div img').setAttribute('src', `${linck_image}`);
-        template_stamping_materials.querySelector('div div img').setAttribute('alt', `hola mundo`);
-        template_stamping_materials.querySelector('div p').textContent = element.name_stamping_size;
+        template_stamping_materials.querySelector('.card_image').setAttribute('src', `${linck_image}`);
+        template_stamping_materials.querySelector('.card_image').setAttribute('alt', `hola mundo`);
+        template_stamping_materials.querySelector('.name_card').textContent = element.name_stamping_size;
         /* True copia toda la estructura interna, si se le pasa un false, solo copia 
         la etiqueta elegida (apertura y cierre)*/
         let clone = document.importNode(template_stamping_materials, true)
