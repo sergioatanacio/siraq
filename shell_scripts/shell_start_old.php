f66cefa6f727de25a5a2f69ed7917c14771afe4e
sergioAtanacio <sergio14camino@gmail.com>
2022-02-21 17:21:51 -0500

    cambios de ubicacion de los directorios

--- shell_scripts/shell_start_old.php
+++ shell_scripts/shell_start_old.php
@@ -1,12 +1,8 @@
 <?php
 
 shell_exec('git pull origin master && git pull origin desarrollo');
+shell_exec('code . && cd public && code . && start php pug_shell_compile_preprocessors.php && start php stylus_shell_compile_preprocessors.php && start php -S localhost:8000 && cd ../ && ls -lha && cd ..');
 
-shell_exec('start pug -w --pretty resourses/preprocessors/pug/public -o ./public');
-
-shell_exec('stylus -w resourses\preprocessors\stylus\first.styl -o .\css\\');
-
-shell_exec('code .  && cd public && php -S localhost:8000 && cd ../ && ls -lha ');
 
 /* 
 1. Ejecutar script SQL de forma directa
