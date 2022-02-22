ab26a5120e3641cf527606d0fca19eb873cb2dd2
sergio14camino <sergio14camino@gmail.com>
2022-02-22 16:07:08 -0500

    Algunos cambios

--- app/app.php
+++ app/app.php
@@ -157,47 +157,7 @@ def($generalController, function($method, $connectionArg, $petition) use ($model
                     'get_stamping_materials' => function() use ($petition, $models, $connectionArg)
                         {
                             #return json_encode($models('stamping_materials', 'get_stamping_materials', $petition, $connectionArg)());
-                            return json_encode(array (
-                                0 => 
-                                    array (
-                                        'id_stamping_materials' => '1',
-                                        'name_of_material' => 'Sublimado',
-                                        'material_images' => 
-                                            array (
-                                                0 => 
-                                                array (
-                                                'image_name' => 'Imagen_AI_cristiana@3x@3x.png',
-                                                'linck_image' => 'pruebas_svg/prueba1.svg',
-                                                ),
-                                            ),
-                                    ),
-                                1 => 
-                                    array (
-                                        'id_stamping_materials' => '2',
-                                        'name_of_material' => 'Vinil textil',
-                                        'material_images' => 
-                                            array (
-                                                0 => 
-                                                array (
-                                                    'image_name' => 'Imagen_AI_cristiana@3x@3x.png',
-                                                    'linck_image' => 'pruebas_svg/prueba2.svg',
-                                                ),
-                                            ),
-                                    ),
-                                2 => 
-                                    array (
-                                        'id_stamping_materials' => '3',
-                                        'name_of_material' => 'Serigrafía',
-                                        'material_images' => 
-                                            array (
-                                                0 => 
-                                                array (
-                                                    'image_name' => 'Imagen_AI_cristiana@3x@3x.png',
-                                                    'linck_image' => 'pruebas_svg/prueba3.svg',
-                                                ),
-                                            ),
-                                    ),
-                                ));
+                            return json_encode(        );
                         },
                     'get_stamping_size' =>  function() use ($petition, $models, $connectionArg)
                         {
